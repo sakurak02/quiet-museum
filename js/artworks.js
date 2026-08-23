013fdb71b40d61686023113f2c488a52ce220333
sakurak02 <kikakuk117@gmail.com>
2026-08-23 13:51:36 +0900

    005を003に

--- js/artworks.js
+++ js/artworks.js
@@ -59,8 +59,8 @@
       image: "images/type-a/A007.webp"
     },
     {
-      id: "B005",
-      image: "images/type-b/B005.webp"
+      id: "B003",
+      image: "images/type-b/B003.webp"
     },
     {
       id: "D003",
